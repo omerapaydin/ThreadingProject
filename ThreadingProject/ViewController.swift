50db69a7ba098d447f9f0c765a87f343b3c7d472
Ömer Apaydın <omerapaydin90@gmail.com>
2024-12-08 16:02:56 +0300

    Initial Commit

---
+++ ThreadingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadingProject
+//
+//  Created by Ömer on 8.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ThreadingProjectTests/ThreadingProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadingProjectTests.swift
+//  ThreadingProjectTests
+//
+//  Created by Ömer on 8.12.2024.
+//
+
+import Testing
+@testable import ThreadingProject
+
+struct ThreadingProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
